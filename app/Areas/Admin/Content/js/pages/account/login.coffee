6d917930e506db91a9cb29ce939464ec3b08df17
Bndy <zb@bndy.net>
2016-12-22 22:15:48 +0800

    missing files

---
+++ app/Areas/Admin/Content/js/pages/account/login.coffee
@@ -0,0 +1,7 @@
+﻿app.controller "LoginCtrl", [
+    "$scope", "$http"
+    ($scope, $http) ->
+        $scope.appSettings = appSettings
+        
+        return
+]
---
+++ app/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Net.Bndy.WebApp.Areas.Admin.Controllers
+{
+    public class AccountController : _ControllerBase
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
