08beafb0469e3403549b65c33aa6ae1215d9e8a0
Bndy <zb@bndy.net>
2017-01-06 23:11:37 +0800

    remove the default value settings when text box has no value

--- app/plugins/daterangepicker/daterangepicker.js
+++ app/plugins/daterangepicker/daterangepicker.js
@@ -281,7 +281,6 @@
                     split = val.split(this.locale.separator);
 
                 start = end = null;
-
                 if (split.length == 2) {
                     start = moment(split[0], this.locale.format);
                     end = moment(split[1], this.locale.format);
@@ -435,12 +434,14 @@
         // if attached to a text input, set the initial value
         //
 
-        if (this.element.is('input') && !this.singleDatePicker && this.autoUpdateInput) {
-            this.element.val(this.startDate.format(this.locale.format) + this.locale.separator + this.endDate.format(this.locale.format));
-            this.element.trigger('change');
-        } else if (this.element.is('input') && this.autoUpdateInput) {
-            this.element.val(this.startDate.format(this.locale.format));
-            this.element.trigger('change');
+        if (this.element.val() != "") {
+            if (this.element.is('input') && !this.singleDatePicker && this.autoUpdateInput) {
+                this.element.val(this.startDate.format(this.locale.format) + this.locale.separator + this.endDate.format(this.locale.format));
+                this.element.trigger('change');
+            } else if (this.element.is('input') && this.autoUpdateInput) {
+                this.element.val(this.startDate.format(this.locale.format));
+                this.element.trigger('change');
+            }
         }
 
     };
